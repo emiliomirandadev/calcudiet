0b22e5e54a059fe2ab6901b293d1d6b1839bd460
Emilio Miranda <emiranda@fidesol.org>
2022-06-16 17:00:14 +0200

    minor changes

---
+++ app/Http/Controllers/DietsController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DietsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $table->foreignId('user_id')->constrained('users');
+        $table->string('comment');
+        $table->integer('totalKcal');
+        $diet[] = request()->except('_token');
+        
+        if($request->hasFile('photo')){
+            $product[0]['photo']=$request->file('photo')->store('uploads','public');
+        }
+        
+        Product::insert($product[0]);
+        $id = Product::where('name',$product[0]['name'])->value('id');
+        //return response()->json($id);
+        
+        return redirect()->route('products.show', ['product' => $id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function diets()
+    {
+        return $this->hasMany(Diet::class);
+    }
 }
